4224843d894b913773b02e187413fbad78afb9a6
Maddie Vagadori <Maddie@Madeleines-MacBook-Pro-8.local>
2017-04-23 23:01:17 -0400

    fixed results page

--- ArabicFlashCards/app/src/main/java/com/example/im_working/arabicflashcards/ResultsActivity.java
+++ ArabicFlashCards/app/src/main/java/com/example/im_working/arabicflashcards/ResultsActivity.java
@@ -1,13 +1,41 @@
 package com.example.im_working.arabicflashcards;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ResultsActivity extends AppCompatActivity {
+public class ResultsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        Button returnToMain = (Button) findViewById(R.id.ReturnToHomeButton);
+
+        returnToMain.setOnClickListener(this);
+
+        Button playAgain = (Button) findViewById(R.id.PlayAgainButton);
+        playAgain.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ReturnToHomeButton:
+                Intent i = new Intent(ResultsActivity.this, MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                break;
+            case R.id.PlayAgainButton:
+                finish();
+                break;
+
+        }
+
+
+    }
+
 }
