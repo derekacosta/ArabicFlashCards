ff38764b5ef4c161637cf4cc226539dca88e1e8d
Maddie Vagadori <Maddie@Madeleines-MacBook-Pro-8.local>
2017-04-23 22:06:42 -0400

    end page

---
+++ ArabicFlashCards/app/src/main/java/com/example/im_working/arabicflashcards/ResultsActivity.java
@@ -0,0 +1,13 @@
+package com.example.im_working.arabicflashcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
